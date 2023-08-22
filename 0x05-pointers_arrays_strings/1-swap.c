211fcee697624c130567ce69d31987c0105c5701
hoodmanb <joshuadebravo@gmail.com>
2023-08-22 06:37:24 -0700

    0x05

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
- * reset_to_98 - swaps the value of two integers
+ * swap_int - swaps the value of two integers
  * @a: int variable
  * @b: int variable
  * return: return 0
  */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-void swap_int(int *a, int *b);
 {
 	int a;
 	int b;
