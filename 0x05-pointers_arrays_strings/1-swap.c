f08fd6556bad2876d2efd9e376fec3d10f38e917
hoodmanb <joshuadebravo@gmail.com>
2023-08-21 18:03:31 -0700

    0x05

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - swaps the value of two integers
@@ -9,6 +10,13 @@
 
 void swap_int(int *a, int *b);
 {
-	*a = *b;
-	*b = temp;
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	printf("%d, %d/n", a, b);
+	swap_int(&a, &b);
+	printf("%d, %d\n", a, b);
+	return (0);
 }
