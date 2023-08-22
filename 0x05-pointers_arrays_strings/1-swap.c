383e35209fd65f682368c32c5447e253cd25f712
hoodmanb <joshuadebravo@gmail.com>
2023-08-22 07:33:58 -0700

    0x05

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -14,15 +14,3 @@ void swap_int(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
-
-{
-	int a;
-	int b;
-
-	a = 98;
-	b = 42;
-	printf("%d, %d/n", a, b);
-	swap_int(&a, &b);
-	printf("%d, %d\n", a, b);
-	return (0);
-}
