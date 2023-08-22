6502045ad4bcdbc45008cad95b82f04bfaf5d0a3
hoodmanb <joshuadebravo@gmail.com>
2023-08-21 17:33:36 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - swaps the value of two integers
+ * @a: int variable
+ * @b: int variable
+ * return: return 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a=*b;
+	*b=temp;
+}
