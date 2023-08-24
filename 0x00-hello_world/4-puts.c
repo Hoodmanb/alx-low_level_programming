cff16b7f435fd646d68eecb74b42fc976eb62d0f
hoodmanb <joshuadebravo@gmail.com>
2023-08-24 15:49:41 -0700

    0x00

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.hi>
+
+/**
+ * main - entry point of the program
+ * Description: uses puts to print the statement
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
