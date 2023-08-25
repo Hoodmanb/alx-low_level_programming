2ff9bd69d3253e945fd4970ef3cf9c81fb9baf3c
hoodmanb <joshuadebravo@gmail.com>
2023-08-24 18:47:00 -0700

    0x00

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ * Description: uses printf to print the statement
+ * Return: 0
+ */
+int main(void)
+{
+	printf("A piece of art,\n");
+	return (0);
+}
