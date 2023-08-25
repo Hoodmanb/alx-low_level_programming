cc2a5effb6c4fddf9973ff59973c05d27b842121
hoodmanb <joshuadebravo@gmail.com>
2023-08-24 19:46:10 -0700

    0x00

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,8 @@ int main(void)
 	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	fprintf(stderr, "[Anything]");
 
 	return (0);
+
+	fprintf(stderr, "[Anything]");
 }
