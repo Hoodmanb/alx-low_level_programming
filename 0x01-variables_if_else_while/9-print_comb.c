b7d03f6362e2c96985aed2214fd223abd57b0573
hoodmanb <joshuadebravo@gmail.com>
2023-08-26 19:40:10 -0700

    variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
-	putchar(i / 10 + '0');
-	putchar(i % 10 + '0');
-
-	if (i < 99)
+	for (i = 0; i < 10; i++)
+	putchar(i + '0');
+	if (i != 9)
 	putchar(',');
 	putchar(' ');
+
+	putchar('\n');
 
 	return (0);
 }
