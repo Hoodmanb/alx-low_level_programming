0445fd66317e25e3ce2a01bf86eeb2738ba772be
hoodmanb <joshuadebravo@gmail.com>
2023-10-18 05:10:04 -0700

    0x05 strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,5 +8,19 @@
 void print_rev(char *s);
 
 {
+int count = 0;
 
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n');
 }
